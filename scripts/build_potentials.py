71ecb9810c62ab8e0f20588433bdff409b82da75
Ricardo <ricardo.fernandes@mpe.au.dk>
2024-12-06 19:26:22 +0100

    Refactored build potentials script so that it can be called programmatically (besides through a terminal/snakemake)

--- scripts/build_potentials.py
+++ scripts/build_potentials.py
@@ -1,48 +1,40 @@
 
 # import necessary Python modules
+from atlite.gis import ExclusionContainer
+from atlite.gis import shape_availability
+from rasterio.plot import show
 import matplotlib.pyplot as plt
 import geopandas
 import pandas
 import atlite
 import logging
-from atlite.gis import ExclusionContainer
-from atlite.gis import shape_availability
-from rasterio.plot import show
+import yaml
 
 
 
-if __name__ == "__main__":
+def build_potentials(config_yaml, network_geojson, corine_dataset, component, csv_file, png_file, log):
 
-    # mock Snakemake in case Python module is called from a terminal
-    if "snakemake" not in globals():
-        from _helpers import mock_snakemake
-
-        snakemake = mock_snakemake("build_potentials")
-
-
-
-    # get component to calculate the potential per node
-    component = snakemake.params["component"]
-
+    # load config yaml file representing the PyPSA-Eur configuration
+    handle = open(config_yaml)
+    config = yaml.safe_load(handle)
+    handle.close()
 
 
     # configure log mechanism
-    logging.basicConfig(level = snakemake.config["logging"]["level"])
-    logger = logging.getLogger(__name__)
-    logger.info("Calculate %s potentials" % component)
+    if log is True:
+        logging.basicConfig(level = config["logging"]["level"])
+        logger = logging.getLogger(__name__)
+        logger.info("Calculate %s potentials" % component)
 
 
-
-    # load geojson file representing the PyPSA-Eur network
-    nodes_geojson = geopandas.read_file(snakemake.input["network_geojson"]).set_index("name")
-
+    # load network geojson file representing the PyPSA-Eur network
+    nodes_geojson = geopandas.read_file(network_geojson).set_index("name")
 
 
-    # select codes (specified in the "config.yaml" file) from Corine Land Cover (CLC) dataset
+    # select CORINE Land Cover (CLC) codes (specified in the config yaml file)
     excluder = ExclusionContainer(crs = 3035)
-    excluder.add_raster(snakemake.input["corine_dataset"], codes = snakemake.config[component]["corine"], invert = True, crs = 3035)
+    excluder.add_raster(corine_dataset, codes = config[component]["corine"], invert = True, crs = 3035)
     cell_area = excluder.res**2
-
 
 
     # calculate potential per node
@@ -51,7 +43,7 @@ if __name__ == "__main__":
         shape = nodes_geojson.to_crs(excluder.crs).loc[[node]].geometry
         band, transform = shape_availability(shape, excluder)
         selected_cells = band.sum() * cell_area / 1e6   # in sqkm
-        potential = selected_cells * snakemake.config[component]["potential_per_sqkm"]
+        potential = selected_cells * config[component]["potential_per_sqkm"]
         df.loc[len(df)] = [node, potential]
         #print("Node=%s" % node)
         #print("Area (km2)=%.2f" % (shape.geometry.area.sum() / 1e6))
@@ -59,22 +51,38 @@ if __name__ == "__main__":
         #print()
 
 
-
     # save potentials into a CSV file
-    logger.info("Save %s potentials into CSV file" % component)
-    df.set_index("node", inplace = True)
-    df.to_csv(snakemake.output["csv_file"])
-
+    if csv_file is not None:
+        if log is True:
+            logger.info("Save %s potentials into CSV file" % component)
+        df.set_index("node", inplace = True)
+        df.to_csv(csv_file)
 
 
     # save potentials into a PNG file
-    logger.info("Save %s potentials into PNG file" % component)
-    shape = nodes_geojson.to_crs(excluder.crs).geometry
-    band, transform = shape_availability(shape, excluder)
-    fig, ax = plt.subplots(figsize = (20, 23))
-    ax.set_axis_off()
-    shape.plot(ax = ax, color = "none")
-    show(band, transform = transform, cmap = "Greens", ax = ax)
-    plt.savefig(snakemake.output["png_file"])
+    if png_file is not None:
+        if log is True:
+            logger.info("Save %s potentials into PNG file" % component)
+        shape = nodes_geojson.to_crs(excluder.crs).geometry
+        band, transform = shape_availability(shape, excluder)
+        fig, ax = plt.subplots(figsize = (20, 23))
+        ax.set_axis_off()
+        shape.plot(ax = ax, color = "none")
+        show(band, transform = transform, cmap = "Greens", ax = ax)
+        plt.savefig(png_file)
 
+
+
+if __name__ == "__main__":
+
+
+    # mock Snakemake in case Python module is called from a terminal
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        snakemake = mock_snakemake("build_potentials")
+
+
+    # build and save potentials into CSV and PNG files
+    build_potentials("config.yaml", snakemake.input["network_geojson"], snakemake.input["corine_dataset"], snakemake.params["component"], snakemake.output["csv_file"], snakemake.output["png_file"], True)
 
